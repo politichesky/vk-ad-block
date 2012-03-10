4bd5939fadf2687f5c653dd666d8eef1c2c4770d
politichesky <rasherny@gmail.com>
2012-03-10 13:04:41 +0400

    replace vkontakte.ru to vk.com

--- vk-ads-block.user.js
+++ vk-ads-block.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Radically simple JS for hide ads from vkontakte.ru. Written for Luakit, not tested extensively in other browsers
 // @author         Sergey Ushakov, rasherny@gmail.com
-// @include        http://*vkontakte.ru/*
+// @include        http://*vk.com/*
 // ==/UserScript==
 
 document.getElementById("left_ads").style.visibility = 'hidden'
